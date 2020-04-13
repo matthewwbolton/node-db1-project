16ad267ad166d15d8b5e8467f540ae9b0c5b454f
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-13 19:51:56 -0400

    commit changes

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const db = require("./data/dbConfig");
 
 const PORT = process.env.PORT || 5000;
 
-server.get("/", (req, res) => {
-  db("products")
-    .then((employees) => {
-      res.status(200).json(employees);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    });
-});
+// enable the following code to test the "products" resource of the northwind database
+
+// server.get("/", (req, res) => {
+//   db("products")
+//     .then((employees) => {
+//       res.status(200).json(employees);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.status(500).json({ error: error.message });
+//     });
+// });
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
