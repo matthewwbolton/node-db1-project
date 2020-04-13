31a1333df267f9a8418bbae7ce81d3ed7d0d7999
Matthew Bolton <matthewwbolton@gmail.com>
2020-04-13 18:44:18 -0400

    restored northwind database with postgres and tested enpoints

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require("dotenv").config();
 const server = require("./api/server.js");
 
+const db = require("./data/dbConfig");
+
 const PORT = process.env.PORT || 5000;
+
+server.get("/", (req, res) => {
+  db("products")
+    .then((employees) => {
+      res.status(200).json(employees);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
